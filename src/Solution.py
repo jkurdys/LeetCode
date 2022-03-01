fabf88c9305ae0678e9dadc9d8b6100d8836c1de
Joshua Kurdys <jkurdys@gmail.com>
2022-03-01 08:33:32 -0700

    Working on Roman to Integer

--- src/Solution.py
+++ src/Solution.py
@@ -18,22 +18,72 @@ An integer is a palindrome when it reads the same backward
 as forward.
 For example, 121 is a palindrome while 123 is not.
 
+13. Roman to Integer
+Roman numerals are represented by seven different symbols: I, V, X,
+L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+
+For example, 2 is written as II in Roman numeral, just two one's added
+together. 12 is written as XII, which is simply X + II. The number 27
+is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to
+right. However, the numeral for four is not IIII. Instead, the number
+four is written as IV. Because the one is before the five we subtract
+it making four. The same principle applies to the number nine, which is
+written as IX. There are six instances where subtraction is used:
+
+    I can be placed before V (5) and X (10) to make 4 and 9. 
+    X can be placed before L (50) and C (100) to make 40 and 90. 
+    C can be placed before D (500) and M (1000) to make 400 and 900.
+
+Given a roman numeral, convert it to an integer.
 '''
 
 class Solution:
-    def threeConsecutiveOdds(self, arr: List[int]) -> bool:
+    def threeConsecutiveOdds(arr):
         for ind in range(len(arr)-2):
             if( arr[ind]%2 == 1 and arr[ind+1]%2 == 1 and arr[ind+2]%2 == 1 ):
                 return True
         return False
 
-    def isPerfectSquare(self, num: int) -> bool:
+    def isPerfectSquare(num):
         if (num**0.5) % 1 == 0:
             return True
         else:
             return False
 
-    def isPalindrome(self, x: int) -> bool:
+    def isPalindrome(x):
         i = str(x)
         j = i[::-1]
         return i == j
+
+    def romanToInt(s):
+        romnums = {'I': 1,
+                   'V': 5,
+                   'X': 10,
+                   'L': 50,
+                   'C': 100,
+                   'D': 500,
+                   'M': 1000}
+        
+        out = romnums[s[-1]]
+        for i in range(len(s) - 2, -1, -1):
+            if s[i + 1] < s[i]:
+                out += romnums[s[i]]
+            else:
+                out -= romnums[s[i]]
+
+        print(out)
+
+if __name__ == '__main__':
+    S = Solution
+    S.romanToInt(s='LVIII')
