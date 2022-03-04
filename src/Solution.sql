f9a39ad4fb057642fc1a52e6c45961c43ebeb3ab
Joshua Kurdys <jkurdys@gmail.com>
2022-03-04 06:03:28 -0700

    Completed Nth Highest Salary challenge.

--- src/Solution.sql
+++ src/Solution.sql
@@ -1,5 +1,6 @@
 /*
-177. Nth Highest SalaryWrite an SQL query to report
+177. Nth Highest Salary
+Write an SQL query to report
 the nth highest salary from the Employee table. If
 there is no nth highest salary, the query should
 report null.
@@ -10,12 +11,15 @@ BEGIN
   RETURN (
       # Write your MySQL query statement below.
       
-          select salary
-          from (
-              select salary,
-                     row_number() over (order by salary desc) as nth
-              from employee
-              ) as sub
-          where n = nth
+          SELECT 
+            CASE WHEN count(1) > 0 THEN salary
+            ELSE NULL 
+            END AS result
+          FROM (
+              SELECT salary,
+                     dense_rank() OVER (ORDER BY salary DESC) AS nth
+              FROM employee
+              ) AS sub
+          WHERE n = nth
   );
 END
